387033f5123c2fccd87ffa48b6959e427e00ac0f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-08-29 18:21:55 +0100

    Initial commit

---
+++ src/iteration/Testes.java
@@ -0,0 +1,38 @@
+package iteration;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Testes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Set<String> uniqueColors=new HashSet<String>();
+List<String> colors=new ArrayList<String>();
+uniqueColors.add("Red");
+colors.add("Red");
+uniqueColors.add("Green");
+colors.add("Green");
+uniqueColors.add("Blue");
+colors.add("Blue");
+uniqueColors.add("Red");
+colors.add("Red");
+System.out.println("parcours de HashSet");
+for(String color: uniqueColors)
+{
+	System.out.println(color);	
+
+}
+System.out.println("parcours de ArrayList");
+for(String cl: colors)
+{
+	System.out.println(cl);	
+
+}
+
+
+	}
+
+}
